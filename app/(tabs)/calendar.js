973e3a44bfd799f620be8ce03227d10215b23e91
Miguel Miguez <miguelmiguezangel@gmail.com>
2025-05-15 11:16:48 -0300

    Update calendar.js

--- app/(tabs)/calendar.js
+++ app/(tabs)/calendar.js
@@ -4,51 +4,16 @@ import { ReserveButton } from "../../components/ReserveButton";
 import { Calendario } from "../../components/Calendario";
 
 const Calendar = () => {
-  const [fromDate, setFromDate] = useState(new Date());
-  const [fromTime, setFromTime] = useState(new Date());
-  const [toDate, setToDate] = useState(new Date());
-  const [toTime, setToTime] = useState(new Date());
-
   const [reservations, setReservations] = useState([]);
 
-  const handleConfirmReservation = ({ fromDate, fromTime, toDate, toTime }) => {
-    // Guardar la reserva en el estado local
-    setReservations((prev) => [
-      ...prev,
-      {
-        from: new Date(
-          fromDate.getFullYear(),
-          fromDate.getMonth(),
-          fromDate.getDate(),
-          fromTime.getHours(),
-          fromTime.getMinutes()
-        ),
-        to: new Date(
-          toDate.getFullYear(),
-          toDate.getMonth(),
-          toDate.getDate(),
-          toTime.getHours(),
-          toTime.getMinutes()
-        ),
-      },
-    ]);
+  const handleConfirmReservation = (from, to) => {
+    setReservations((prev) => [...prev, { from, to }]);
   };
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <Calendario reservations={reservations} />
-
-      <ReserveButton
-        fromDate={fromDate}
-        fromTime={fromTime}
-        toDate={toDate}
-        toTime={toTime}
-        setFromDate={setFromDate}
-        setFromTime={setFromTime}
-        setToDate={setToDate}
-        setToTime={setToTime}
-        onReserve={handleConfirmReservation}
-      />
+      <ReserveButton onReserve={handleConfirmReservation} />
     </ScrollView>
   );
 };
