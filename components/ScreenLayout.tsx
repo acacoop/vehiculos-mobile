7a165d5d0083b4f87e1aa7056b0027a4a0275196
Miguel Miguez <138310942+MiguelMiguez@users.noreply.github.com>
2026-03-17 11:04:03 -0300

    Update ScreenLayout.tsx

--- components/ScreenLayout.tsx
+++ components/ScreenLayout.tsx
@@ -42,6 +42,7 @@ export const ScreenLayout: React.FC<ScreenLayoutProps> = ({
         options={{
           headerTitle: title,
           headerTitleAlign: "center",
+          headerBackTitle: "",
         }}
       />
       {renderContent()}
